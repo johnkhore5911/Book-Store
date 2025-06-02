8b5e60882225b5167efc5e6656011ddd0886b523
johnkhore5911 <johnkhore26@gmail.com>
2025-06-02 20:06:21 +0530

    first commit

---
+++ controllers/bookController.js
@@ -0,0 +1,61 @@
+const Book = require('../models/Book');
+
+// GET all books
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// POST a new book
+const createBook = async (req, res) => {
+  const { title, author, price, publishedDate } = req.body;
+
+  const book = new Book({
+    title,
+    author,
+    price,
+    publishedDate
+  });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// PUT (update) a book by ID
+const updateBook = async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// DELETE a book by ID
+const deleteBook = async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getAllBooks,
+  createBook,
+  updateBook,
+  deleteBook
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bookRoutes = require('./routes/bookRoutes');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/books', bookRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.error('MongoDB connection error:', err));
